56cdf36c271467ccc1c44e5002b93f7be4288974
ProjectBatch18 <83127293+ProjectBatch18@users.noreply.github.com>
2021-04-26 13:38:25 +0530

    Add files via upload

---
+++ SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.application;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Setting extends PreferenceActivity {
+	
+	public void onCreate(Bundle b)
+	{
+		super.onCreate(b);
+		addPreferencesFromResource(R.xml.settings_activity);
+	}
+
+}
